88f516503e094c54605007648b68c2212c231eb2
toddmnorton <toddmnorton@live.com>
2020-09-11 12:46:07 -0500

    felt good about this one

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -4,27 +4,33 @@
 //////////////////STEP 1////////////////////
 //Create a variable called 'lovesCode' and assign it a value of true or false (as a boolean) depending on if you love code or not. (It's okay if you don't yet!)
 
-// Code Here
+const lovesCode = true;
 
 //////////////////STEP 2////////////////////
 //Create a variable called 'age' that has a value equal to your age as a number. 
 
-// Code Here
+const age = 35;
 
 //////////////////STEP 3////////////////////
 //Create a variable called 'name' and assign it a value of your name as a string.  
 
-// Code Here
+const name = 'Todd'
 
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
-// Code Here
+const phrase = 'Here we go'
 
-//////////////////STEP 5////////////////////
+//////////////////STEP 5////////////////////    
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
-// Code Here
+function codePoem(lovesCode) {
+    if (lovesCode === true) {
+        phrase.push('I love code')
+    } else {
+        phrase.push('I do not love code')
+    }
+}
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
